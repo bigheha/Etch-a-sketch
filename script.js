cb58c1b7694793998dcc30cc65ad7a30c9c09595
bigheha <fofo@mail.com>
2022-11-06 23:15:01 +0200

    initial commit, implemented script for grid-creation

---
+++ script.js
@@ -0,0 +1,12 @@
+const container = document.querySelector('.container');
+
+for (i = 0; i < 16; i++) {
+    const row = document.createElement('div');
+    row.classList.add('row');
+    for (j = 0; j < 16; j++) {
+        const box = document.createElement('div');
+        box.classList.add('box');
+        row.appendChild(box);
+    }
+    container.appendChild(row);
+}
