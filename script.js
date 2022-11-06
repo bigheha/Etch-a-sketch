f74cdcfad1fd9350feee92394d9e0704c889628e
bigheha <fofo@mail.com>
2022-11-06 23:38:30 +0200

    added drawing on hover

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 const container = document.querySelector('.container');
 
+function changeColor(a) {
+    a.style.backgroundColor = 'red';
+}
+
 for (i = 0; i < 16; i++) {
     const row = document.createElement('div');
     row.classList.add('row');
     for (j = 0; j < 16; j++) {
         const box = document.createElement('div');
+        box.addEventListener('mouseover', () => changeColor(box));
         box.classList.add('box');
         row.appendChild(box);
     }
