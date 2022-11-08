38a3f78562bd6edf35e32f4a2d9c750bc71c8c0c
bigheha <fofo@mail.com>
2022-11-08 11:15:26 +0200

    finished grid

--- script.js
+++ script.js
@@ -1,17 +1,39 @@
 const container = document.querySelector('.container');
 
+function deleteGrid() {
+    const rows = document.querySelectorAll('div.row');
+    rows.forEach(row => container.removeChild(row));
+}
+
+function drawGrid(gridSize) {
+    for (i = 0; i < gridSize; i++) {
+        const row = document.createElement('div');
+        row.classList.add('row');
+        for (j = 0; j < gridSize; j++) {
+            const box = document.createElement('div');
+            box.addEventListener('mouseover', () => changeColor(box));
+            box.classList.add('box');
+            row.appendChild(box);
+        }
+        container.appendChild(row);
+    }
+}
+
 function changeColor(a) {
     a.style.backgroundColor = 'red';
 }
 
-for (i = 0; i < 16; i++) {
-    const row = document.createElement('div');
-    row.classList.add('row');
-    for (j = 0; j < 16; j++) {
-        const box = document.createElement('div');
-        box.addEventListener('mouseover', () => changeColor(box));
-        box.classList.add('box');
-        row.appendChild(box);
-    }
-    container.appendChild(row);
+let size = 16;
+
+function setSize() {
+    deleteGrid();
+    do {
+        size = prompt('Input size of grid (less than 100)');
+    }while(size > 100 || size < 1);
+    drawGrid(size);
+
 }
+
+document.querySelector('button').addEventListener('click', () => setSize());
+
+drawGrid(size);
